2bf16330fa223c6ae792f7fe071b9d8708c47058
kenoshiro <ohshiro.ken.6e@kyoto-u.ac.jp>
2023-11-07 18:36:12 +0900

    revision

--- prog/plot.R
+++ prog/plot.R
@@ -1,12 +1,15 @@
 mytheme <- list()
 mytheme$theme_set1 <- theme_bw()+theme(text=element_text(family='Helvetica'),
-                               plot.title=element_text(size=11),
-                               panel.grid=element_blank(),
-                               panel.border=element_blank(),
-                               axis.title=element_text(size=10),
-                               axis.line=element_line(color='black',linewidth=0.3),
-                               axis.ticks=element_line(color='black',linewidth=.3),
-                               strip.background=element_blank())
+                                       plot.title=element_text(size=8.5),
+                                       panel.grid=element_blank(),
+                                       panel.border=element_blank(),
+                                       axis.line=element_line(color='black',linewidth=.3),
+                                       axis.ticks=element_line(color='black',linewidth=.3),
+                                       axis.text=element_text(size=8),
+                                       axis.title=element_text(size=9),
+                                       legend.title=element_text(size=9),
+                                       legend.text=element_text(size=8),
+                                       strip.background=element_blank())
 lst$scenario <- c('NZE-CR','NZE-AF','NZE-LD','NZE-IM','NZE-ET','NDC80')
 df$scen_col <- c('NZE-CR'='#66C2A5','NZE-AF'='#FC8D62','NZE-LD'='#8DA0CB','NZE-IM'='#E78AC3','NZE-ET'='#A6D854','NDC80'='#B3B3B3')
 
@@ -33,7 +36,7 @@ p$emi_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     geom_bar(aes(x=Scenario,y=Value,fill=Variable,color=Variable),position='stack',stat='identity')+
     geom_hline(yintercept=0,color='grey20',linewidth=.3)+
     labs(x=NULL,y=expression(paste('GHG emissions (Mt-',CO[2],'eq ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col_fill,labels=leg,name=NULL)+
     scale_color_manual(values=col_line,labels=leg,name=NULL)+
     guides(fill=guide_legend(title=NULL))
@@ -74,7 +77,7 @@ p$ccs_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     geom_hline(yintercept=0,color='grey20',linewidth=.3)+
     labs(x=NULL,y=expression(paste('CCUS (Mt-',CO[2],' ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',strip.background=element_blank(),axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',strip.background=element_blank(),axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 df$var <- tribble(~Variable,~Legend,~Color,
@@ -88,15 +91,16 @@ p$cdr_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     ggplot()+
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     labs(x=NULL,y=expression(paste('CDR (Mt-',CO[2],' ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',strip.background=element_blank(),axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',strip.background=element_blank(),axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 p$l1 <- get_legend(p$emi_CO2_sec+theme(legend.position='right'))
 p$tmp1 <- plot_grid(p$emi_2050,p$emi_CO2_sec+theme(legend.position='none'),
-                    nrow=1,axis='l',align='vh',rel_widths=c(1,1.3),labels=c('a','b'))
-p$tmp2 <- plot_grid(p$ccs_2050,p$cdr_2050,p$l1,nrow=1,axis='tb',align='h',rel_widths=c(1,1,.3),labels=c('c','d',''))
-p$tmp <- plot_grid(p$tmp1,p$tmp2,ncol=1,rel_heights=c(1,.8))
-ggsave(filename='output/fig1.png',plot=p$tmp,width=200,height=150,units='mm',dpi=300,bg='white')
+                    nrow=1,axis='l',align='vh',rel_widths=c(1,1.3),labels=c('a','b'),label_size=12)
+p$tmp2 <- plot_grid(p$ccs_2050,p$cdr_2050,p$l1,nrow=1,axis='tb',align='h',rel_widths=c(1,1,.3),labels=c('c','d',''),label_size=12)
+p$tmp <- plot_grid(p$tmp1,p$tmp2,ncol=1,rel_heights=c(1,.9))
+ggsave(filename='output/fig1.png',plot=p$tmp,width=190,height=130,units='mm',dpi=300,bg='white')
+ggsave(filename='output/fig1.eps',plot=p$tmp,width=190,height=130,units='mm')
 
 
 # Fig.2 -------------------------------------------------------------------
@@ -120,7 +124,7 @@ p$fec_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     ylim(0,13.5)+
     labs(title=NULL,x=NULL,y=expression(paste('Final energy (EJ ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 df$var  <- tribble(~Variable,~Legend,~Color,
@@ -197,7 +201,7 @@ p$fin_ene_path <- filter(df$all,Variable=='Fin_Ene') %>%
     geom_point(aes(x=Year,y=Value,color=Scenario),shape=21,fill='white')+
     ylim(0,13.5)+
     labs(title=NULL,x=NULL,y=expression(paste('Final energy (EJ ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position=c(.3,.4),legend.background=element_blank())+
+    mytheme$theme_set1+theme(legend.position='right',legend.background=element_blank())+
     scale_color_manual(values=df$scen_col,name=NULL)+
     guides(color=guide_legend(title=NULL))
 
@@ -253,16 +257,19 @@ p$fin_ene_tra_ele <- filter(df$all,Year==2050) %>%
     scale_color_manual(values=df$scen_col,name=NULL)+
     guides(color=guide_legend(title=NULL))
 
-p$tmp1 <- plot_grid(p$fin_ene_path,
-                    p$fec_2050+theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.line.y=element_blank(),axis.title.y=element_blank()),
-                    p$fin_ene_ele+theme(legend.position='none'),
-                    nrow=1,rel_widths=c(1,.9,1),axis='tb',align='h',labels=c('a','','b'))
+p$l_fin_ene_path <- get_legend(p$fin_ene_path)
+p$l_fec_2050 <- get_legend(p$fec_2050)
+p$tmp1 <- plot_grid(p$fin_ene_path+theme(legend.position='none'),
+                    p$fec_2050+theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.line.y=element_blank(),axis.title.y=element_blank(),legend.position='none'),
+                    p$fin_ene_ele+theme(legend.position='none'),p$l_fin_ene_path,p$l_fec_2050,
+                    nrow=1,rel_widths=c(1,.5,1,.45,.45),axis='tb',align='h',labels=c('a','','b'),label_size=12)
 p$fec_sec <- plot_grid(p$fec_ind+theme(legend.position='none'),p$fin_ene_ind_ele+theme(legend.position='none'),
                        p$fec_bui+theme(legend.position='none'),p$fin_ene_bui_ele+theme(legend.position='none'),
                        p$fec_tra+theme(legend.position='none'),p$fin_ene_tra_ele+theme(legend.position='none'),
-                       nrow=1,rel_widths=c(1,.7,1,.7,1,.7),axis='tb',align='h',labels=c('c','','d','','e',''))
-p$tmp <- plot_grid(p$tmp1,p$fec_sec,ncol=1,rel_heights=c(1,.9),labels=c('a',''))
-ggsave(filename='output/fig2.png',plot=p$tmp,bg='white',width=200,height=140,units='mm',dpi=300)
+                       nrow=1,rel_widths=c(1,.7,1,.7,1,.7),axis='tb',align='h',labels=c('c','','d','','e',''),label_size=12)
+p$tmp <- plot_grid(p$tmp1,p$fec_sec,ncol=1,rel_heights=c(1,1))
+ggsave(filename='output/fig2.png',plot=p$tmp,bg='white',width=190,height=130,units='mm',dpi=300)
+ggsave(filename='output/fig2.eps',plot=p$tmp,width=190,height=130,units='mm')
 
 
 # Fig.3 -------------------------------------------------------------------
@@ -305,7 +312,7 @@ p$sec_ele_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     ylim(0,8.5)+
     labs(title=NULL,x=NULL,y=expression(paste('Power generation (EJ ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 df$var <- tribble(~Variable,~Legend,~Color,
@@ -326,12 +333,16 @@ p$sec_hyd_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     ggplot()+
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     labs(title=NULL,x=NULL,y=expression(paste('Hydrogen supply (EJ ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
-p$tmp <- plot_grid(p$sec_ele_path+theme(legend.position='none'),p$sec_ele_2050+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.line.y=element_blank(),axis.ticks.y=element_blank()),
-                   p$sec_hyd_2050,nrow=1,rel_widths=c(1,1.3,1.5),axis='tb',align='h',labels=c('a','','b'))
-ggsave(filename='output/fig3.png',plot=p$tmp,width=200,height=85,units='mm',dpi=300)
+p$l_tmp <- plot_grid(get_legend(p$sec_hyd_2050),get_legend(p$sec_ele_path),ncol=1)
+p$tmp <- plot_grid(p$sec_ele_path+theme(legend.position='none'),
+                   p$sec_ele_2050+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.line.y=element_blank(),axis.ticks.y=element_blank()),
+                   p$sec_hyd_2050+theme(legend.position='none'),p$l_tmp,
+                   nrow=1,rel_widths=c(1,1.4,.8,.8),axis='tb',align='h',labels=c('a','','b'),label_size=12)
+ggsave(filename='output/fig3.png',plot=p$tmp,width=190,height=75,units='mm',dpi=300)
+ggsave(filename='output/fig3.eps',plot=p$tmp,width=190,height=75,units='mm')
 
 
 # Fig.4 -------------------------------------------------------------------
@@ -348,7 +359,7 @@ df$var <- tribble(~Variable,~Legend,~Color,
                   'Prm_Ene_Win','Wind','lightskyblue3',
                   'Prm_Ene_Oce','Ocean','paleturquoise3',
                   'Prm_Ene_Oth','Other','grey',
-                  'Prm_Ene_Sec_Ene_Trd','Secondary energy\nimport','thistle2')
+                  'Prm_Ene_Sec_Ene_Trd','Secondary energy import','thistle2')
 leg <- as.character(df$var$Legend); names(leg) <- as.character(df$var$Variable)
 col <- as.character(df$var$Color); names(col) <- as.character(df$var$Variable)
 p$prm_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>% 
@@ -357,7 +368,7 @@ p$prm_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     ggplot()+
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     labs(title=NULL,x=NULL,y=expression(paste('Primary energy (EJ ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 p$imp_path <- filter(df$all,Variable=='Imp_Dep_Prm_Ene') %>% 
@@ -398,7 +409,7 @@ p$imp2 <- filter(df$all,Variable%in%df$var$Variable) %>%
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     ylim(0,200)+
     labs(title=NULL,x=NULL,y=NULL)+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),axis.text.y=element_blank(),axis.line.y=element_blank(),axis.ticks.y=element_blank())+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),axis.text.y=element_blank(),axis.line.y=element_blank(),axis.ticks.y=element_blank(),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 p$imp_cos_path <- filter(df$all,Variable=='Imp_Ene_and_Emi') %>% 
@@ -414,17 +425,18 @@ p$imp_cos_path <- filter(df$all,Variable=='Imp_Ene_and_Emi') %>%
     guides(color=guide_legend(title=NULL))
 
 p$l1 <- get_legend(p$prm_2050+guides(fill=guide_legend(ncol=3)))
-p$tmp1 <- plot_grid(p$imp_path+theme(legend.position=c(.25,.3),legend.background=element_blank()),
+p$tmp1 <- plot_grid(p$imp_path+theme(legend.position=c(.25,.35),legend.background=element_blank()),
                     p$prm_2050+theme(legend.position='none'),
-                    nrow=1,rel_widths=c(1,.7),axis='tb',align='h',labels=c('a',''))
+                    nrow=1,rel_widths=c(1,.7),axis='tb',align='h',labels=c('a',''),label_size=12)
 p$tmp2 <- plot_grid(p$tmp1,p$l1,ncol=1,rel_heights=c(1,.3))
 
 p$l1 <- get_legend(p$imp2+guides(fill=guide_legend(nrow=2)))
 p$tmp1 <- plot_grid(p$imp_cos_path+theme(legend.position='none'),p$imp2+theme(legend.position='none'),
-                    nrow=1,rel_widths=c(1,.55),axis='tb',align='h',labels=c('b',''))
+                    nrow=1,rel_widths=c(1,.55),axis='tb',align='h',labels=c('b',''),label_size=12)
 p$tmp3 <- plot_grid(p$tmp1,p$l1,ncol=1,rel_heights=c(1,.2))
 p$tmp <- plot_grid(p$tmp2,p$tmp3,nrow=1,rel_widths=c(1,.85))
-ggsave(filename='output/fig4.png',plot=p$tmp,bg='white',width=200,height=110,units='mm',dpi=300)
+ggsave(filename='output/fig4.png',plot=p$tmp,bg='white',width=190,height=100,units='mm',dpi=300)
+ggsave(filename='output/fig4.eps',plot=p$tmp,bg='white',width=190,height=100,units='mm')
 
 
 # Fig.5 -------------------------------------------------------------------
@@ -451,7 +463,7 @@ p$cum_inv <- filter(df$all,Variable=='Pol_Cos_Cum_Dis') %>%
 df$var <- tribble(~Variable,~Legend,~Color,
                   'Inv_Add_Ene_Sup_Ele','Electricity','lightsteelblue',
                   'Inv_Add_Ene_Sup_Hyd','Hydrogen','thistle2',
-                  'Inv_Add_Ene_Sup_Oth','Other energy\nsupply','moccasin',
+                  'Inv_Add_Ene_Sup_Oth','Other energy supply','moccasin',
                   'Inv_Add_Ene_Dem_Ind','Industry','salmon',
                   'Inv_Add_Ene_Dem_Res_and_Com','Buildings','lightskyblue3',
                   'Inv_Add_Ene_Dem_Tra','Transport','darkolivegreen2',
@@ -464,9 +476,10 @@ p$inv_2050 <- filter(df$all,Variable%in%df$var$Variable,Year==2050) %>%
     ggplot()+
     geom_bar(aes(x=Scenario,y=Value,fill=Variable),position='stack',stat='identity')+
     labs(title=NULL,x=NULL,y=expression(paste('Investment (billion US$ ',yr^{`-1`},')')))+
-    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1))+
+    mytheme$theme_set1+theme(legend.position='right',axis.text.x=element_text(angle=45,hjust=1),legend.key.size=unit(5,'mm'))+
     scale_fill_manual(values=col,labels=leg,name=NULL)
 
 p$tmp <- plot_grid(p$car_pri+theme(legend.position=c(.3,.75)),p$cum_inv+theme(legend.position='none'),p$inv_2050,
-                   nrow=1,axis='tb',align='h',labels=c('a','b','c'),rel_widths=c(1,.8,1.3))
-ggsave(filename='output/fig5.png',plot=p$tmp,width=200,height=80,units='mm',dpi=300)
+                   nrow=1,axis='tb',align='h',labels=c('a','b','c'),rel_widths=c(1,.8,1.4),label_size=12)
+ggsave(filename='output/fig5.png',plot=p$tmp,width=190,height=75,units='mm',dpi=300)
+ggsave(filename='output/fig5.eps',plot=p$tmp,width=190,height=75,units='mm')
